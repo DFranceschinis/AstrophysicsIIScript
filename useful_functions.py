b6fb6d71dd0bea4a3927206c405c08166e4332fb
klj11 <a1704505@student.adelaide.edu.au>
2018-08-20 15:29:58 +0930

    Added file called useful_functions.py to put all useful calculation functions into. Already has function to calculate the solid angle of a beam

---
+++ useful_functions.py
@@ -0,0 +1,12 @@
+#create a funtion to calculate the solid angle of a region. The function takes 
+# an angle from the origin in degrees and returns the solid angle of that area
+def solid_angle(theta):
+	import numpy as np 
+	import math
+
+	theta = math.radians(theta)
+
+	#solid angle formula
+	d_omega = 2*np.pi*(1-np.cos(theta))
+
+	return(d_omega)	
