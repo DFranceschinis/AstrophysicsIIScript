469ad9de05fb7dd02def63d657cf0306b92eb2ac
DFranceschinis <42332357+DFranceschinis@users.noreply.github.com>
2018-08-20 18:36:32 +0930

    Changed tabs and added features
    I changed my tab style so its compatible with others.
    
    Changed the data from a bunch of lists to a list of classes.
    
    Added count region functions

--- AstroScript.py
+++ AstroScript.py
@@ -1,28 +1,28 @@
 """
-        Astrophysics II Neutrino Practical Module
-        Contributors:   Shayne Doddrell         
-                        Daniel Franceschinis    
-                        Kendall Jenner          
-                        Muskan Pathak           
-                        Gary Hill               
+		Astrophysics II Neutrino Practical Module
+		Contributors:   Shayne Doddrell         
+						Daniel Franceschinis    
+						Kendall Jenner          
+						Muskan Pathak           
+						Gary Hill               
 
-        This is the module that encompasses all the code that will be used for the practical. 
-        If this document becomes unwieldy, it can be split into multiple files.
-        If managing this project becomes unwieldy, it can be maintained on github.
-        Don't be afraid to study this document. You might learn some python!
+		This is the module that encompasses all the code that will be used for the practical. 
+		If this document becomes unwieldy, it can be split into multiple files.
+		If managing this project becomes unwieldy, it can be maintained on github.
+		Don't be afraid to study this document. You might learn some python!
 
-        I recommend editing this document using sublime text!
-            https://www.sublimetext.com/3
+		I recommend editing this document using sublime text!
+			https://www.sublimetext.com/3
 
 
 
-        This can be run using the following processes.
-        Make sure that this script is in the same folder as the files downloaded from this source
-            https://icecube.wisc.edu/science/data/TXS0506_point_source
+		This can be run using the following processes.
+		Make sure that this script is in the same folder as the files downloaded from this source
+			https://icecube.wisc.edu/science/data/TXS0506_point_source
 
-        If you're using the terminal. Make sure that you are in the correct directory, by typing:
-            cd [DIR]
-        where [DIR] is the directory that you've placed the script and the downloaded files
+		If you're using the terminal. Make sure that you are in the correct directory, by typing:
+			cd [DIR]
+		where [DIR] is the directory that you've placed the script and the downloaded files
 
 		There are two ways you can run the script. The first way will just produce the plots straight up.
 		The second way is useful if you wish to be able to play with the data and work with the script rather than
@@ -33,43 +33,46 @@
 												might also need to type AstroScript.py instead, depends on the system.]
 
 		Second Way: Type into your terminal the following:
-            python                          [This will open up python for you, giving you some information,
-                                                It should say python 3.x. If it says python 2.7,
-                                                exit using quit() and type python3 instead]
-            import AstroScript              [This will import the module]
-            AstroScript.run()               [This will actually do all the running]
-        That should work.
+			python                          [This will open up python for you, giving you some information,
+												It should say python 3.x. If it says python 2.7,
+												exit using quit() and type python3 instead]
+			import AstroScript              [This will import the module]
+			AstroScript.run()               [This will actually do all the running]
+		That should work.
 
 
-        If you're using IDLE. Run these commands.
-            import sys
-            sys.path.append("[DIR]")
+		If you're using IDLE. Run these commands.
+			import sys
+			sys.path.append("[DIR]")
 
-        [DIR] needs to be the path where your files are located. If you're on windows, there will be
-        slashes as part of your directory, ex: \Astrophysics II\Practical\Group, these need replaced with
-        double slashes, ex: \\Astrophysics II\\Practical\\Group.
-        Don't forget the quotes!
+		[DIR] needs to be the path where your files are located. If you're on windows, there will be
+		slashes as part of your directory, ex: \Astrophysics II\Practical\Group, these need replaced with
+		double slashes, ex: \\Astrophysics II\\Practical\\Group.
+		Don't forget the quotes!
 
-        Once that is done. Run these commands:
-            import AstroScript
-            AstroScript.run()
+		Once that is done. Run these commands:
+			import AstroScript
+			AstroScript.run()
 
 
 
-        If you can't get this all working, email me or complain to me in person [Daniel].
+		If you can't get this all working, email me or complain to me in person [Daniel].
 
 
 ###
 
-        Things to do:
-            Play with the plotting to make the data look prettier.
-            Start writing functions for analysing the data containers (ex: for the )
+		Things to do:
+			Play with the plotting to make the data look prettier.
+			Start writing functions for analysing the data containers (ex: for the )
 
 
 ###
 
 """
-form useful_functions import *
+### Useful functions that we will regularly use go in this file. This is mainly for organisational purposes.
+
+from useful_functions import *
+
 ### Set up the location of the files. Make sure the files are in the same directory as this script.
 
 import os
@@ -88,77 +91,70 @@ NAMES.append("events_IC86a.txt")
 NAMES.append("events_IC86b.txt")
 NAMES.append("events_IC86c.txt")
 
-###
+### Contain the coordinates for the proposed source
 
 ORIGIN_RA = 77.3582
 ORIGIN_DEC = 5.69314
 
+### Class for the data points
+
+class neutrino(object):
+
+	def __init__(self, MJD, RA, DEC, UNC, ENERGY):
+		self.MJD = MJD
+		self.RA = RA
+		self.DEC = DEC
+		self.UNC = UNC
+		self.ENERGY = ENERGY
+		#self.separation = find_angular_separation(ORIGIN_RA, ORIGIN_DEC, self.RA, self.DEC)
+
+	# def __repr__(self):
+	#     return ''.join("Neutrino Data Point: ","\tMJD: ",self.MJD,"\tRA: ",self.RA,"\tDEC",self.DEC,"\tUNC",self.UNC,"ENERGY: ",self.log10)
+
 ### Data Containers that are used to store the data that is processed.
 
-MJD = []
-RA = []
-DEC = []
-UNC = []
-log10 = []
-separations = []
+NEUTRINOS = []
 
 ###
 
-
 def process_all_files():
-    """ This function will do all the data processing.
-    """
+	""" This function will do all the data processing.
+	"""
 
-    #   Note, this function is local to this function, thus it won't have a name conflict with other functions.
+	#   Note, this function is local to this function, thus it won't have a name conflict with other functions.
 
-    #   Clean up previous runs
+	#   Clean up previous runs
 
-    MJD.clear()
-    RA.clear()
-    DEC.clear()
-    UNC.clear()
-    log10.clear()
+	NEUTRINOS.clear()
+	def process(mylist, lines, number):
+		for i in lines:
+			mylist.append(float((i.split())[number]))
 
-    def process(mylist, lines, number):
-        for i in lines:
-            mylist.append(float((i.split())[number]))
+	def process_the_file(name):
+		FILE = open(os.path.join(__location__,"data", name), 'r')
+		header = FILE.readline()
+		for i in FILE:
+			M,R,D,U,l = i.strip().split()
+			NEUTRINOS.append(neutrino(M,R,D,U,l))
+		
 
-    def process_the_file(name):
-        FILE = open(os.path.join(__location__,"data", name), 'r')
-        header = FILE.readline()
-        for i in FILE:
-            M,R,D,U,l = i.strip().split()
-            MJD.append(M)
-            RA.append(R)
-            DEC.append(D)
-            UNC.append(U)
-            log10.append(l)
-            separations.append(find_angular_separation(ORIGIN_RA, ORIGIN_DEC, R, D))
-        
-
-    for file in NAMES:
-        process_the_file(file)
+	for file in NAMES:
+		process_the_file(file)
 
 
 def plot():
-    
-    import matplotlib.pyplot as plt
+	
+	import matplotlib.pyplot as plt
 
-    plt.scatter(MJD, log10)
-    plt.title("Luminosity vs MJD (I think...)")
-    plt.xlabel("MJD")
-    plt.ylabel("Luminosity [log10]")
-    plt.show()
+	MJD = [neut.MJD for neut in NEUTRINOS]
+	EN = [neut.ENERGY for neut in NEUTRINOS]
 
-def find_angular_separation(origin_RA, origin_DEC, point_RA, point_DEC):
-    import astropy.units as u
-    from astropy.coordinates import SkyCoord
+	plt.scatter(MJD, EN)
+	plt.title("Energy vs MJD")
+	plt.xlabel("MJD")
+	plt.ylabel("Energy [log10]")
+	plt.show()
 
-    origin_coord = SkyCoord(origin_RA*u.deg, origin_DEC*u.deg)
-    point_coord = SkyCoord(point_RA*u.deg, point_DEC*u.deg)
-
-
-    return origin_coord.separation(point_coord)
 
 def sky_map():
 	import matplotlib
@@ -166,15 +162,18 @@ def sky_map():
 	import numpy as np
 	from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-	coords = []
-	coords.append((RA, DEC))
+	RA = [neut.RA for neut in NEUTRINOS]
+	DEC = [neut.DEC for neut in NEUTRINOS]
+	EN = [neut.ENERGY for neut in NEUTRINOS]
+	coords = [(R, D) for R in RA for D in DEC]
+	
 
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
 	divider = make_axes_locatable(ax)
 	cax = divider.append_axes('right', size='5%', pad=0.05)
 
-	smap = ax.scatter(RA, DEC, label='IceCube Data', c = log10, cmap = 'BuPu', s = 50)
+	smap = ax.scatter(RA, DEC, label='IceCube Data', c = EN, cmap = 'BuPu', s = 50)
 	ax.plot(ORIGIN_RA, ORIGIN_DEC, 'ro', label='TXS0506+056')
 	ax.legend(loc="upper right")
 	ax.set(title='Right Ascension and Declination of Data Readings', xlabel='Right Ascension (Degrees)', ylabel='Declination (Degrees)')
@@ -183,11 +182,10 @@ def sky_map():
 
 ### run this function to actually DO the stuff.
 def run():
-    
 
-    process_all_files()
-    plot()
-    sky_map()
+	process_all_files()
+	plot()
+	sky_map()
 
 #	This allows the code to run if you just use $ python AstroScript
 if __name__ == '__main__':
--- useful_functions.py
+++ useful_functions.py
@@ -1,5 +1,5 @@
-#create a funtion to calculate the solid angle of a region. The function takes 
-# an angle from the origin in degrees and returns the solid angle of that area
+#	create a funtion to calculate the solid angle of a region. The function takes 
+# 	an angle from the origin in degrees and returns the solid angle of that area
 def solid_angle(theta):
 	import numpy as np 
 	import math
@@ -11,14 +11,14 @@ def solid_angle(theta):
 
 	return(d_omega)	
 
-#solid_angle_strip is a function which calculates the solid angle of a strip of sky.
-#It takes in a scalar value theta1 which is the angle in degrees from vertical to 
-# the top of the strip, scalar value theta2 which is the angle in degrees from
-# vertical to the bottom of the strip, and scalar value extent which is the extent 
-# of the strip in degrees.
-#The function uses the function solid_angle
-#The function returns the solid angle of that strip.
-def solid_angle_strip(theta1, theta2, extent, abs=True)
+#	solid_angle_strip is a function which calculates the solid angle of a strip of sky.
+#	It takes in a scalar value theta1 which is the angle in degrees from vertical to 
+# 	the top of the strip, scalar value theta2 which is the angle in degrees from
+# 	vertical to the bottom of the strip, and scalar value extent which is the extent 
+# 	of the strip in degrees.
+#	The function uses the function solid_angle
+#	The function returns the solid angle of that strip.
+def solid_angle_strip(theta1, theta2, extent, abs = True):
 	import numpy as np
 	import math
 
@@ -27,3 +27,39 @@ def solid_angle_strip(theta1, theta2, extent, abs=True)
 		d_omega = math.abs(d_omega)
 
 	return(d_omega)
+
+###	This will take an origin point and find all the points (pass as list) that are within max_angle of them
+###	Returns a list of all the points. Iterate over the list if you want to do stuff with them.
+###	Take the length of the list if you want to count them
+def find_points_within_angle(origin_RA, origin_DEC, points, max_angle):
+	matches = []
+
+	for p in points:
+		if find_angular_separation(origin_RA, origin_DEC, p.RA, p.DEC) <= max_angle:
+			matches.append(p)
+
+	return matches
+
+###	This will take two angles for the inner and outer edges of the strip and the points
+###	This will return a list of the points that are in the region.
+###	Note, the angles are the inner/outer. The order doesn't matter.
+def find_points_in_strip(theta1, theta2, points):
+	matches = []
+
+	for p in points:
+		if (p.DEC >= theta1 and p.DEC <= theta2) or (p.DEC <= theta1 and p.DEC >= theta2):
+			matches.append(p)
+
+	return matches
+
+###	Finds the angular separation between two points.
+###	Fairly self documenting.
+def find_angular_separation(origin_RA, origin_DEC, point_RA, point_DEC):
+	import astropy.units as u
+	from astropy.coordinates import SkyCoord
+
+	origin_coord = SkyCoord(origin_RA*u.deg, origin_DEC*u.deg)
+	point_coord = SkyCoord(point_RA*u.deg, point_DEC*u.deg)
+
+
+	return origin_coord.separation(point_coord)
